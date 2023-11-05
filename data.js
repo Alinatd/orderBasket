5b8fd6e35fb2a18b5687ac6413b215357dba317b
Alina Turdakunova <141915076+Alinatd@users.noreply.github.com>
2023-11-05 19:26:30 +0600

    Add files via upload

---
+++ data.js
@@ -0,0 +1,46 @@
+const menu_items = [
+    {
+        id:1,
+    title: 'Hamburger',
+    price: 200,
+    img :'./images/burger.png'
+},
+{
+    id:2,
+    title: 'Sandwich',
+    price: 180,
+    img :'./images/sandwich.png'
+},
+{
+    id:3,
+    title: 'Manty',
+    price: 250,
+    img :'./images/dumplings.png'
+},
+{
+    id:4,
+    title: 'Lagman',
+    price: 220,
+    img :'./images/lagman.png'
+},
+{
+    id:5,
+    title: 'Juice',
+    price: 100,
+    img :'./images/juice.png'
+},
+{
+    id:6,
+    title: 'Coca-Cola',
+    price: 120,
+    img :'./images/coke.png'
+},
+{
+    id:7,
+    title: 'Coffee',
+    price: 100,
+    img :'./images/cofeee.png'
+}
+]
+
+const orders_basket = []
